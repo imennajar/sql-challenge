a092475cd0e98c19c566b6f7b74bbc51ba375f0b
imennajar <imen668669@gmail.com>
2023-08-16 23:51:18 -0400

    last update

--- EmployeeSQL/table schema.sql
+++ EmployeeSQL/table schema.sql
@@ -3,44 +3,41 @@ CREATE TABLE departments (
 	dept_name VARCHAR(50) NOT NULL UNIQUE
 );
 
-
 CREATE TABLE titles(
-  title_id VARCHAR(5) PRIMARY KEY,
-  title VARCHAR (50) NOT NULL UNIQUE
-  
+	title_id VARCHAR(5) PRIMARY KEY,
+	title VARCHAR (50) NOT NULL UNIQUE  
 );
 
 CREATE TABLE employees (
-	 emp_no INT PRIMARY KEY,
-	 emp_title_id VARCHAR(5) NOT NULL,
-     birth_date DATE NOT NULL,
-     first_name VARCHAR(50) NOT NULL,
-     last_name VARCHAR(50) NOT NULL,
-     sex CHAR,
-     hire_date DATE NOT NULL,
-     FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR(5) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	sex CHAR,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
 );
 
 CREATE TABLE dept_emp(
-  emp_no INT NOT NULL, 
-  dept_no VARCHAR(4) NOT NULL,
-  PRIMARY KEY (emp_no, dept_no),
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+	emp_no INT NOT NULL, 
+	dept_no VARCHAR(4) NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
-
 CREATE TABLE dept_manager (
-    dept_no VARCHAR(4)  NOT NULL,
-    emp_no INT NOT NULL,
-    PRIMARY KEY (emp_no, dept_no),
+	dept_no VARCHAR(4)  NOT NULL,
+	emp_no INT NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
-
 CREATE TABLE salaries (
-  emp_no INT primary key,
-  salary INT NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	emp_no INT primary key,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
+
